2bed96604954be12662a2ad9fc5fc82d25d8a020
daniilmishin01 <daniilmishin01@yandex.ru>
2026-01-19 16:32:15 +0300

    Replace Djoser JWT URLs with Simple JWT views

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from api.views import (
     CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
@@ -17,5 +22,7 @@ router.register(
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/jwt/', include('djoser.urls.jwt')),
+    path('v1/jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('v1/jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('v1/jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
 ]
