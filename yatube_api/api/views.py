c95634d72acb8f89a9223b6e64f6d2a60f22c164
daniilmishin01 <daniilmishin01@yandex.ru>
2026-01-19 16:06:57 +0300

    Add explicit validation handling in TokenCreateView

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -63,3 +63,8 @@ class TokenCreateView(DjoserTokenCreateView):
     def get_serializer_class(self):
         from api.serializers import CustomTokenCreateSerializer
         return CustomTokenCreateSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return super().post(request, *args, **kwargs)
